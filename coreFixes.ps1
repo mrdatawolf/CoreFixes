ef2eb6ac388e9924d3a2e9ac27e6971c13857fc7
Patrick Moon <patrickmoon@outlook.com>
2024-03-05 13:31:36 -0800

    fixes admin uac update

--- coreFixes.ps1
+++ coreFixes.ps1
@@ -12,7 +12,12 @@ notes
 
 #>
 #Patrick Moon - 2024
-Start-Process -FilePath "powershell" -ArgumentList "-File .\coreFixes.ps1" -Verb RunAs
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    # orginal: Start-Process -FilePath "powershell" -ArgumentList "-File .\coreSetup.ps1" -Verb RunAs
+    # We are not running as administrator, so start a new process with 'RunAs'
+    Start-Process powershell.exe "-File",($myinvocation.MyCommand.Definition) -Verb RunAs
+    exit
+}
 
 $global:errors=0;
 function Invoke-Sanity-Checks {
